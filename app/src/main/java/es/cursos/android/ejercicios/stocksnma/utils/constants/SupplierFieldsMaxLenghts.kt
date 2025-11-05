1695669bf954e8a27365b709c76de85e15820803
Nerea Moraleda <nereaalmansa03@gmail.com>
2025-11-05 12:03:52 +0100

    Modificación de nombres

---
+++ app/src/main/java/es/cursos/android/ejercicios/stocksnma/utils/constants/SupplierFieldsMaxLenghts.kt
@@ -0,0 +1,12 @@
+package es.cursos.android.ejercicios.stocksnma.utils.constants
+
+object SupplierFieldsMaxLenghts {
+    const val NAME_MAX_LENGTH = 100
+    const val CONTACT_NAME_MAX_LENGTH = 80
+    const val EMAIL_MAX_LENGTH = 120
+    const val PHONE_MAX_LENGTH = 20
+    const val ADDRESS_MAX_LENGTH = 150
+    const val CITY_MAX_LENGTH = 60
+    const val COUNTRY_MAX_LENGTH = 60
+    const val ZIP_CODE_MAX_LENGTH = 15
+}
