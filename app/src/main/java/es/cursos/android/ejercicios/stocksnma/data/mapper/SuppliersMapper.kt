75841b6fad61fb285b3ce18ad04db34d6c039a28
Nerea Moraleda <nereaalmansa03@gmail.com>
2025-11-05 12:06:38 +0100

    Agregación de Mappers necesarios para Suppliers

--- app/src/main/java/es/cursos/android/ejercicios/stocksnma/data/mapper/SuppliersMapper.kt
+++ app/src/main/java/es/cursos/android/ejercicios/stocksnma/data/mapper/SuppliersMapper.kt
@@ -2,10 +2,15 @@ package es.cursos.android.ejercicios.stocksnma.data.mapper
 
 import es.cursos.android.ejercicios.stocksnma.data.local.entity.SupplierEntity
 import es.cursos.android.ejercicios.stocksnma.data.remote.dto.supplier.SupplierDto
+import es.cursos.android.ejercicios.stocksnma.data.remote.dto.supplier.SupplierHomeViewDto
+import es.cursos.android.ejercicios.stocksnma.data.remote.dto.supplier.SupplierSelectionMenuDto
 import es.cursos.android.ejercicios.stocksnma.domain.model.Supplier
+import es.cursos.android.ejercicios.stocksnma.domain.model.SupplierHomeView
+import es.cursos.android.ejercicios.stocksnma.domain.model.SupplierSelectionMenu
 
+// SupplierDto -> Supplier
 fun SupplierDto.toSupplier(): Supplier = Supplier(
-    id = id.toString(), // TODO - Hasta eliminar todo SupplierEntity mantener Supplier con id como String
+    id = id,
     name = name,
     contactName = contactName ?: "",
     email = email ?: "",
@@ -17,25 +22,45 @@ fun SupplierDto.toSupplier(): Supplier = Supplier(
     isActive = isActive
 )
 
+// Supplier -> SupplierDto
+fun Supplier.toSupplierDto(): SupplierDto = SupplierDto(
+    id = id,
+    name = name,
+    contactName = contactName.ifBlank { null },
+    email = email.ifBlank { null },
+    phone = phone.ifBlank { null },
+    address = address.ifBlank { null },
+    city = city,
+    country = country,
+    zipCode = zipCode.ifBlank { null },
+    isActive = isActive
+)
+
+
+
+// SupplierHomeViewDto -> SupplierHomeView
+fun SupplierHomeViewDto.toSupplierHomeView(): SupplierHomeView = SupplierHomeView(
+    id = id,
+    name = name,
+    contactName = contactName ?: "",
+    email = email ?: "",
+    phone = phone ?: ""
+    //isActive = isActive
+)
+
+// SupplierSelectionMenuDto -> SupplierSelectionMenu
+fun SupplierSelectionMenuDto.toSupplierSelectionMenu(): SupplierSelectionMenu = SupplierSelectionMenu(
+    id = id,
+    name = name
+)
+
+
 
 fun Supplier.toSupplierEntity(): SupplierEntity = SupplierEntity(
-    id = id,
+    id = id.toString(),
     name = name,
     contactName = contactName,
     email = email,
     phone = phone,
     address = address,
-)
-
-
-fun SupplierEntity.toSupplier(): Supplier = Supplier(
-    id = id,
-    name = name,
-    contactName = contactName ?: "",
-    email = email ?: "",
-    phone = phone ?: "",
-    address = address ?: ""
-//    city = "",
-//    country = "",
-//    zipCode = ""
 )
